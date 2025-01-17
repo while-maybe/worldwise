b8a8308544b3ff6303dfb1e131ab92625ff12ce8
username <178353419+while-maybe@users.noreply.github.com>
2025-01-17 02:25:12 +0000

    feat: add geoLocation functionality
    add useGeolocation hook;

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,13 +11,18 @@ import {
 
 import styles from "./Map.module.css";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "../components/Button";
 
 function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParam] = useSearchParams();
+  const {
+    position: geolocationPosition,
+    isLoading: isLoadingPosition,
+    getPosition,
+  } = useGeolocation();
 
   // we can't access the search params directly, a get is needed
   const mapLat = searchParam.get("lat");
@@ -30,8 +35,21 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading" : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
